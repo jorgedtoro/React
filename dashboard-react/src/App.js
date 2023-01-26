23e549d60db08c9a620ec9b771a395f549405282
Jorge de Toro <72794927+jorgedtoro@users.noreply.github.com>
2023-01-26 15:11:23 +0100

    add components

--- dashboard-react/src/App.js
+++ dashboard-react/src/App.js
@@ -1,7 +1,54 @@
-import "./App.css";
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { Routes, Route } from "react-router-dom";
+import { ProSidebarProvider } from "react-pro-sidebar";
+import Topbar from "./scenes/global/Topbar";
+import Dashboard from "./scenes/dashboard";
+import Sidebar from "./scenes/global/Sidebar";
+// import Team from "./scenes/team";
+// import Invoices from "./scenes/invoices";
+// import Contacts from "./scenes/contacts";
+// import Bar from "./scenes/bar";
+// import Form from "./scenes/form";
+// import Line from "./scenes/line";
+// import Pie from "./scenes/pie";
+// import FAQ from "./scenes/faq";
+// import Geography from "./scenes/geography";
+// import Calendar from "./scenes/calendar";
 
 function App() {
-  return <div className="app"></div>;
+  const [theme, colorMode] = useMode();
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <ProSidebarProvider>
+            <Sidebar />
+            <main className="content">
+              <Topbar />
+
+              <Routes>
+                <Route path="/" element={<Dashboard />} />
+                {/* <Route path="/team" element={<Team />} /> */}
+                {/* <Route path="/contact" element={<Contacts />} /> */}
+                {/* <Route path="/invoices" element={<Invoices />} /> */}
+                {/* <Route path="/form" element={<Form />} /> */}
+                {/* <Route path="/bar" element={<Bar />} /> */}
+                {/* <Route path="/pie" element={<Pie />} /> */}
+                {/* <Route path="/line" element={<Line />} /> */}
+                {/* <Route path="/faq" element={<FAQ />} /> */}
+                {/* <Route path="/geography" element={<Geography />} /> */}
+                {/* <Route path="/calendar" element={<Calendar />} /> */}
+              </Routes>
+            </main>
+          </ProSidebarProvider>
+        </div>
+        ;
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
 }
 
 export default App;
